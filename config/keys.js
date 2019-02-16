9d8306447d6d0e88613ee514fee1f4c2e2028009
JRivenburgh <jurassicjosh429@gmail.com>
2019-02-15 19:46:06 -0800

    here

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://jurassicjosh:b74TC4OA3YVVPAHI@eyeout-shard-00-00-ngqhj.mongodb.net:27017,eyeout-shard-00-01-ngqhj.mongodb.net:27017,eyeout-shard-00-02-ngqhj.mongodb.net:27017/User?ssl=true&replicaSet=EyeOut-shard-0&authSource=admin&retryWrites=true",
+    secretOrKey: "secret"
+};
